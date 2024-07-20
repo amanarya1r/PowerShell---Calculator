002b7701fab00646bbd0a244536da6cc8e5ca5a8
Aman Arya <162666090+amanarya1r@users.noreply.github.com>
2024-07-21 00:45:34 +0530

    Calculator using powershell script

---
+++ Calculator.ps1
@@ -0,0 +1,34 @@
+﻿#----------------Calculator---------------------------
+[int]$a=Read-host -Prompt 'Enter 1st Number'
+[char]$b=Read-host -Prompt 'Enter Operation You Want (+,-,*,%,/)'
+[int]$c=Read-host -Prompt 'Enter 2nd Number'
+[int]$add=$a+$c
+[int]$sub=$a-$c
+[int]$multiple=$a*$c
+[double]$divide=$a/$c
+[int]$modulus=$a%$c
+
+switch($b)
+{
+    +{$result = 'You choose addition'
+      write-Host("Your answer is $add")
+      break;
+      }
+    -{$result = 'You choose Substraction'
+      write-Host("Your answer is $sub")
+      break;
+      }
+    *{$result = 'You choose Multiplication'
+      write-Host("Your answer is $multiple")
+      break;
+      }
+    %{$result = 'You choose Modulus'
+      write-Host("Your answer is $modulus")
+      break;
+      }
+    +{$result = 'You choose Divide'
+      write-Host("Your answer is $divide")
+      break;
+      }
+}
+$result
